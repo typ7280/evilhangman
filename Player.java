f53abfbd2db0766ca5540158a1b08513a33b09cc
petcharat1280 <petcharat1280@users.noreply.github.com>
2017-04-02 23:51:09 +0700

    Java Code

---
+++ Player.java
@@ -0,0 +1,20 @@
+
+
+
+public class Player extends System{
+    int length;
+    char letter;
+    
+    public static void main(){
+        
+    }
+    
+    public void inputLength(int number){
+        
+    }
+    
+    public char guess(char letter){
+        return letter;
+    }
+    
+}
---
+++ Word_Temp.java
@@ -0,0 +1,11 @@
+
+import java.util.ArrayList;
+
+public class Word_Temp extends System{
+    String word;
+    ArrayList<String> letterList;
+    
+    public void findWord(String temp){
+        
+    }
+}
